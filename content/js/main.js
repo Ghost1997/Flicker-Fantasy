5f83c8dbe3ca9fdeebfc46663ca1b9e4a7a14868
Sujit Kumar <mailatsujit@gmail.com>
2023-08-15 00:04:21 +0530

    updated main file

--- content/js/main.js
+++ content/js/main.js
@@ -49,10 +49,10 @@
       $dropdown.off("mouseenter mouseleave");
     }
   });
-  // const host = "https://restaurant-gmpb.onrender.com";
-  // const rKey = "rzp_test_OdnawryqhvxPn7";
-  const host = "http://localhost:3000";
-  const rKey = "rzp_test_3VHA6PauX0jlhZ";
+  const host = "https://restaurant-gmpb.onrender.com";
+  const rKey = "rzp_test_OdnawryqhvxPn7";
+  // const host = "http://localhost:3000";
+  // const rKey = "rzp_test_3VHA6PauX0jlhZ";
   const datePicker = document.getElementById("datepicker");
   const slotContainer = document.getElementById("slotContainer");
   const theaterRadios = document.querySelectorAll("[data-theater]");
