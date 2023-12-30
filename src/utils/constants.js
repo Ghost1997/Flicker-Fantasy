cf14f441120279d3941d5f95d09a4e4165f53f8b
Sujit Kumar <mailatsujit@gmail.com>
2023-12-30 11:37:25 +0530

    stage commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,28 +1,15 @@
 const pricingInfo = {
-  executive: 499,
-  couple: 599,
-  standerd: 599,
+  one: 1299,
+  couple: 1199,
+  two: 1299,
 };
 
 const theaterType = {
-  0: "executive",
-  1: "standerd",
+  0: "one",
+  1: "two",
   2: "couple",
 };
 
-const cakePricingInfo = {
-  blackForest: 500,
-  butterScotch: 500,
-  chocolate: 500,
-  pineApple: 500,
-  roundRedVelvet: 600,
-  buleBerry: 600,
-  mangoCake: 600,
-  heartRedVelvet: 600,
-  deathByChocolate: 700,
-  chocoAlmond: 750,
-  heartPinata: 850,
-};
 const cakeName = {
   blackForest: "Black Forest",
   butterScotch: "Butter Scotch",
@@ -65,58 +52,37 @@ const slotInfo = [
     slots: [
       {
         id: 0,
-        value: "09:00 - 10:45",
+        value: "09:30 - 12:30",
         _id: {
           $oid: "64cdd777f2e9bc2fdb53c5cd",
         },
       },
       {
         id: 1,
-        value: "11:00 - 12:45",
+        value: "13:00 - 16:00",
         _id: {
           $oid: "64cdd777f2e9bc2fdb53c5ce",
         },
       },
       {
         id: 2,
-        value: "13:00 - 14:45",
+        value: "16:15 - 19:15",
         _id: {
           $oid: "64cdd777f2e9bc2fdb53c5cf",
         },
       },
       {
         id: 3,
-        value: "15:00 - 16:45",
+        value: "19:30 - 22:30",
         _id: {
           $oid: "64cdd777f2e9bc2fdb53c5d0",
         },
       },
       {
         id: 4,
-        value: "17:00 - 18:45",
+        value: "22:30 - 01:30",
         _id: {
           $oid: "64cdd777f2e9bc2fdb53c5d1",
-        },
-      },
-      {
-        id: 5,
-        value: "19:00 - 20:45",
-        _id: {
-          $oid: "64cdd777f2e9bc2fdb53c5d2",
-        },
-      },
-      {
-        id: 6,
-        value: "21:00 - 22:45",
-        _id: {
-          $oid: "64cdd777f2e9bc2fdb53c5d3",
-        },
-      },
-      {
-        id: 7,
-        value: "23:00 - 00:45",
-        _id: {
-          $oid: "64cdd777f2e9bc2fdb53c5d4",
         },
       },
     ],
@@ -126,21 +92,21 @@ const slotInfo = [
     slots: [
       {
         id: 0,
-        value: "09:00 - 12:00",
+        value: "09:30 - 12:30",
         _id: {
           $oid: "64cdd777f2e9bc2fdb53c5d6",
         },
       },
       {
         id: 1,
-        value: "12:30 - 15:30",
+        value: "13:00 - 16:00",
         _id: {
           $oid: "64cdd777f2e9bc2fdb53c5d7",
         },
       },
       {
         id: 2,
-        value: "16:00 - 19:00",
+        value: "16:15 - 19:15",
         _id: {
           $oid: "64cdd777f2e9bc2fdb53c5d8",
         },
@@ -152,6 +118,13 @@ const slotInfo = [
           $oid: "64cdd777f2e9bc2fdb53c5d9",
         },
       },
+      {
+        id: 4,
+        value: "22:30 - 01:30",
+        _id: {
+          $oid: "64cdd778f2e9bc2fdb53c5d9",
+        },
+      },
     ],
   },
   {
@@ -159,28 +132,28 @@ const slotInfo = [
     slots: [
       {
         id: 0,
-        value: "09:00 - 12:00",
+        value: "09:00 - 12:30",
         _id: {
           $oid: "64cdd777f2e9bc2fdb53c5db",
         },
       },
       {
         id: 1,
-        value: "12:30 - 15:30",
+        value: "13:00- 14:00",
         _id: {
           $oid: "64cdd777f2e9bc2fdb53c5dc",
         },
       },
       {
         id: 2,
-        value: "16:00 - 17:45",
+        value: "16:15 - 19:15",
         _id: {
           $oid: "64cdd777f2e9bc2fdb53c5dd",
         },
       },
       {
         id: 3,
-        value: "18:00 - 21:00",
+        value: "19:30 - 23:30",
         _id: {
           $oid: "64cdd777f2e9bc2fdb53c5de",
         },
@@ -195,4 +168,4 @@ const slotInfo = [
     ],
   },
 ];
-module.exports = { advanceDecorationPrice, pricingInfo, theaterType, decoration, decorationPrice, cakePricingInfo, cakeName, emailSubject, slotInfo };
+module.exports = { advanceDecorationPrice, pricingInfo, theaterType, decoration, decorationPrice, cakeName, emailSubject, slotInfo };
