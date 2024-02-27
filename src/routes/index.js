ae5b782ba1b119d2968d99a3851870fd4d1ab35a
Sujit Kumar <mailatsujit@gmail.com>
2024-02-27 22:25:01 +0530

    created health check api

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { addOnController } = require("../controllers/index");
 
+router.get("/heathcheck", (req, res) => {
+  return res.status(200).json({ status: "OK" });
+});
 router.post("/addAddon", addOnController.addAddon);
 router.post("/addItemsInAddon", addOnController.addItemsInAddon);
 router.get("/getAddon", addOnController.getAddon);
